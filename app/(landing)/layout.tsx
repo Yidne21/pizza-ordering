b3dc16fadb98b02ef1e2c39ff2a93bc1429ad18c
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2024-10-24 20:33:28 +0300

    fix: order history layout to fit the screen size properly

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Navbar from "@/components/Home/navbar";
+import { Box } from "@mui/material";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
       <Navbar />
       {children}
-    </>
+    </Box>
   );
 }
 
